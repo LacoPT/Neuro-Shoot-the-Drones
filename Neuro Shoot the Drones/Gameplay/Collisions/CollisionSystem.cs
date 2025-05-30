6ac14cfb2e90447be7ac59c60a997b86879b970e
LacoPT <LacoPT@LACO-LAPTOP>
2025-05-30 13:07:40 +0500

    Collision sytem update based on using quadtree

--- Neuro Shoot the Drones/Gameplay/Collisions/CollisionSystem.cs
+++ Neuro Shoot the Drones/Gameplay/Collisions/CollisionSystem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,38 +9,86 @@ namespace Neuro_Shoot_the_Drones.Gameplay.Collisions
 {
     internal class CollisionSystem
     {
-        List<CollisionComponent> Colliders = new();
+        private readonly Rectangle worldBounds;
+        private readonly List<CollisionComponent> colliders = new List<CollisionComponent>();
+        private readonly List<CollisionComponent> collidersToRemove = new List<CollisionComponent>();
+
+        private HashSet<(CollisionComponent, CollisionComponent)> processedPairs = new HashSet<(CollisionComponent, CollisionComponent)>();
+
+        public CollisionSystem(Rectangle worldBounds)
+        {
+            this.worldBounds = worldBounds;
+        }
 
         public void Update()
         {
-            for(int i = 0; i < Colliders.Count; i++)
+            foreach (var collider in collidersToRemove)
+                colliders.Remove(collider);
+            Quadtree quadtree = new Quadtree(0, worldBounds);
+            foreach (var collider in colliders)
             {
-                for(int j = i+1; j< Colliders.Count; j++)
+                quadtree.Insert(collider);
+            }
+
+            processedPairs.Clear();
+
+            foreach (var col in colliders)
+            {
+                List<CollisionComponent> potentialColliders = new List<CollisionComponent>();
+                quadtree.Retrieve(potentialColliders, col);
+
+                foreach (var other in potentialColliders)
                 {
-                    var col1 = Colliders[i];
-                    var col2 = Colliders[j];
-                    if( (col1.ImpactLayers & col2.AffectedByLayers) != 0 && CheckCollisions(col1, col2) )
-                        col2.RegisterCollision(col1.CollisionData);
-                    if((col2.ImpactLayers & col1.AffectedByLayers) != 0 && CheckCollisions(col1, col2) )
-                        col1.RegisterCollision(col2.CollisionData);
+                    if (col == other)
+                        continue;
+
+                    if (IsPairProcessed(col, other))
+                        continue;
+
+                    MarkPairProcessed(col, other);
+
+                    if (CheckCollision(col, other))
+                    {
+                        if ((col.ImpactLayers & other.AffectedByLayers) != 0)
+                        {
+                            other.RegisterCollision(col.CollisionData);
+                        }
+                        if ((other.ImpactLayers & col.AffectedByLayers) != 0)
+                        {
+                            col.RegisterCollision(other.CollisionData);
+                        }
+                    }
                 }
             }
         }
 
-        public void AddCollider(CollisionComponent col)
+        private bool CheckCollision(CollisionComponent col1, CollisionComponent col2)
         {
-            Colliders.Add(col);
+            Vector2 delta = col1.Position - col2.Position;
+            float distanceSquared = delta.LengthSquared();
+            float combinedRadius = col1.HitCircleSize + col2.HitCircleSize;
+            return distanceSquared < combinedRadius * combinedRadius;
         }
 
-        public void RemoveCollider(CollisionComponent col)
+        private bool IsPairProcessed(CollisionComponent col1, CollisionComponent col2)
         {
-            Colliders.Remove(col);
+            return processedPairs.Contains((col1, col2)) || processedPairs.Contains((col2, col1));
         }
 
-        //NOTE: Maybe later i'll introduce different CollisionShape, but now it is how it is
-        bool CheckCollisions(CollisionComponent col1, CollisionComponent col2)
+        private void MarkPairProcessed(CollisionComponent col1, CollisionComponent col2)
         {
-            return (col1.Position - col2.Position).Length() < (col1.HitCircleSize + col2.HitCircleSize);
+            processedPairs.Add((col1, col2));
+        }
+
+        public void AddCollider(CollisionComponent collider)
+        {
+            if (!colliders.Contains(collider))
+                colliders.Add(collider);
+        }
+
+        public void RemoveCollider(CollisionComponent collider)
+        {
+            collidersToRemove.Add(collider);
         }
     }
 }
